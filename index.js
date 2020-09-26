17ef89fd7d735d32695b22ffae290c34157b7eec
Kimberly Sampson <kbburk54@yahoo.com>
2020-09-26 06:10:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 function getFirstSelector(selector){
   return document.querySelector(selector);
 }
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+   // (i + 1).toString();
+  }
+  return lis;
+}
+
+/*
+const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + 1).toString();
+}*/
+
+
 
